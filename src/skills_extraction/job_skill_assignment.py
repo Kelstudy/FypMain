e77cef0f17631a23ddda30d33a4ebe94cc836796
kelvin work <kelvinstudy@outlook.com>
2026-01-23 11:48:42 +0000

    Get job info and define fuzzy search

--- src/skills_extraction/job_skill_assignment.py
+++ src/skills_extraction/job_skill_assignment.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 from pathlib import Path
-import rapidfuzz
+from rapidfuzz import process,utils,fuzz
 
 #Setup paths
 BaseDir = Path(__file__).parents[2] 
@@ -15,14 +15,46 @@ def main():
         return
     
     adzunaDF = pd.read_excel(fileIn)
-    escoLibraryDF = pd.read_excel(escoLibraryIn)
+    escoLibraryDF = pd.read_parquet(escoLibraryIn)
 
     #get top 500 most common skills of all jobs for later skill extraction from job descriptions
     commonSkills = escoLibraryDF["preferredLabel_skill"].value_counts().head(500).index.tolist()
     # get unique job titles for later use
     escoTitles = escoLibraryDF["preferredLabel_job"].unique()
 
-    #Use to store jobs and skills final results
+    #Use to store final results for jobs plus their skills
     results = []
+
+    print (f"Refining matches for {len(adzunaDF)} jobs...")
+
+    #get job information
+    for i , row in adzunaDF.itterows():
+        jobID = row.get("id","N/A")
+        rawTitle = str(row["title"])
+        roleGroup = str(row.get("search_keyword", "Unknown")).lower()
+        description = str(row["description"]).lower()
+        contractType = str(row["contract_type"])
+        postingUrl = str(row["redirect_url"])
+        salaryMin = float(row["salary_min"])
+        salaryMax = float(row["salary_max"])
+
+
+    # Use FuzzySearch to find closest ESCO job title compared to Adzuna job title
+    fuzzyMatchResult = process.extractOne(
+        rawTitle,
+        escoTitles,
+        scorer = fuzz.WRatio, # WRatio chosen as found best for general purpose
+        processor = utils.default_process  # remove non-alphanumeric, trim whitespaces, convert to lower case
+    )
+
+
+
+    
+
+
+        
+
+
+
 
     
