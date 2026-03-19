dee7bf69139a39b0027aa981f1689388cd020df0
kelvin rushbrook <kelvinstudy@outlook.com>
2026-03-19 12:46:05 +0000

    Created Streamlit app code file and imported functions

--- src/app.py
+++ src/app.py
@@ -8,14 +8,20 @@ import sys
 st.set_page_config(page_title="Job Skill Matcher", layout="wide") 
 
 #Set project root folder
-root = Path(__file__).resolve().parents[1]  # FypMain/
+rootPath = Path(__file__).resolve().parents[1]  # FypMain/
 
 #add root folder to pythons search list and set to search there first for imports below
-sys.path.insert(0, str(root))
+sys.path.insert(0, str(rootPath))
 
 #Import data pipeline functions
 from src.data_pipeline.data_pull import main as adzunaPull
 from src.data_pipeline.ESCO_combine import buildTechSkillLibrary
 from src.data_pipeline.job_skill_assignment import main as assignSkills
 
-
+# Function to check if the ESCO library exists and create it if not
+def ensureEscoLibraryExists():
+    escoFile = rootPath / "data" / "processed" / "job_skills_library.parquet"
+    if not escoFile.exists():
+        print("Setup: Building ESCO library for the first time...")
+        buildTechSkillLibrary()
+    return True
